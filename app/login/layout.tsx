367a211e0d2287b59298c2c8d108c4014cd86006
moathssnck <myneww00@gmail.com>
2025-07-14 13:36:23 -0600

    sasa

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../globals.css'
 import { ToastProvider } from '@/components/ui/toast'
 
 const inter = Inter({ subsets: ['latin'] })
